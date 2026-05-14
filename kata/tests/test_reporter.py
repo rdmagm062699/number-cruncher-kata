f995794e5ad91a8da795715d3b20360165dda104
Rob Miller <rob.miller@pediatrix.com>
2026-05-14 15:03:34 -0500

    chore: more simplifications

--- kata/tests/test_reporter.py
+++ kata/tests/test_reporter.py
@@ -1,17 +1,7 @@
 import pytest
-import unittest
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 from src.reporter import crunch_report
 
-# Hint: patch the name as it is imported into reporter, not where it is defined.
-#   @patch("src.reporter.crunch_the_numbers")
 
-
-class TestCrunchReport(unittest.TestCase):
-
-    def test_placeholder(self):
-        self.fail("Replace this with your first real test")
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_placeholder():
+    pytest.fail("Replace this with your first real test")
